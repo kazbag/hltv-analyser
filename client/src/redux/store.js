62b06a1210c3ab01e905f522611433e8a4cce6e3
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-08-12 14:45:49 +0200

    added logger middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import matchesReducers from "./matches/matchesReducers";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
