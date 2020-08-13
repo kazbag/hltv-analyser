c6d92ab93ccecbcdcf5f8553e8e1d2acbff5826b
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-08-13 13:59:26 +0200

    included logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
-import matchesReducers from "./matches/matchesReducers";
 import rootReducer from "./rootReducer";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
