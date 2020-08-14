41457e46778ba582ffc09f6d41c12d514963e7c3
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-08-14 11:05:40 +0200

    installed and fixed cors error

--- api/main.js
+++ api/main.js
@@ -1,12 +1,13 @@
 const env = require("dotenv").config();
 const path = require("path");
+const cors = require("cors");
 const PORT = process.env.API_PORT || 3001;
 
 const matchesRoute = require("./routes/matches");
 
 const express = require("express");
 const app = express();
-
+app.use(cors());
 app.set("view-engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
