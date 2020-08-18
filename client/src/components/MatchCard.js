a146d31050866d4c214bf6ce7ee2520670320e37
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-08-18 09:54:23 +0200

    added links to teams

--- client/src/components/MatchCard.js
+++ client/src/components/MatchCard.js
@@ -7,7 +7,7 @@ const MatchCard = (props) => {
       <h6 className="heading heading--darken heading--title--small heading-6 match__heading">
         {event ? event.name : "TBA"}
       </h6>
-      <p className="paragraph--small item__date">
+      <p className="paragraph--small match__date">
         {date && !live ? new Date(date).toLocaleString() : "playing"}
       </p>
       <p
@@ -26,14 +26,26 @@ const MatchCard = (props) => {
         {team1 ? team1.name + " vs " : "TBA vs "}
         {team2 ? team2.name : "TBA"}
       </p>
-      <div className="match__links">
-        <a
-          className="match__link match__link--team match__link--stream"
-          href={`https://hltv.org/team/`}
-        >
-          Team 1
-        </a>
-      </div>
+      {team1 && team1.id && team2 && team2.id ? (
+        <div className="match__links">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="match__link match__link--team match__link--team1"
+            href={`https://hltv.org/team/${team1.id}/${team1.name}`}
+          >
+            {team1.name}
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="match__link match__link--team match__link--team2"
+            href={`https://hltv.org/team/${team2.id}/${team2.name}`}
+          >
+            {team2.name}
+          </a>
+        </div>
+      ) : null}
     </li>
   );
 };
