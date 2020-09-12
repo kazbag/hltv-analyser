196ba85818ccbe9d2be9985d9f6afdb960af7164
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-09-12 14:19:57 +0200

    handling post loading

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Basic from "../components/Form/Basic";
 import axios from "axios";
 import { apiUri } from "../cfg";
-
-const handleUserDataRequest = (userData) => {
-  const { email, password, target } = userData;
-  console.log(userData);
-  const user = {
-    email,
-    password,
-  };
-  axios
-    .post(`${apiUri}/api/${target}`, { user })
-    .then((response) => {
-      console.log(response.data.token);
-      if (response.data.token) {
-        localStorage.clear();
-        localStorage.setItem("token", response.data.token);
-      }
-    })
-    .catch((err) => console.log(err.message));
-};
+import Loading from "../components/Loading";
 
 const LoginPage = () => {
+  const [loading, setLoading] = useState(false);
+  const handleUserDataRequest = (userData) => {
+    setLoading(true);
+    const { email, password, target } = userData;
+    const user = {
+      email,
+      password,
+    };
+    axios
+      .post(`${apiUri}/api/${target}`, { user })
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.clear();
+          localStorage.setItem("token", response.data.token);
+        }
+      })
+      .catch((err) => console.log(err.message))
+      .finally(() => setLoading(false));
+  };
   return (
     <section className="section section__login">
       <h3 className="heading heading--title heading-3">
         Log in or create an account
       </h3>
       <Basic handleUserDataRequest={handleUserDataRequest} />
+      {loading && <Loading />}
     </section>
   );
 };
